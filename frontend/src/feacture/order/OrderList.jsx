da5a8a79eb195f2674425ffd1c2fd40e76f782f1
kk250639 <chesyeo@gmail.com>
2025-07-24 12:11:49 +0900

    오더리스트 수정/재작성

--- frontend/src/feacture/order/OrderList.jsx
+++ frontend/src/feacture/order/OrderList.jsx
@@ -1,6 +1,29 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import { Col, FormGroup, FormLabel, Row, Table } from "react-bootstrap";
+import { useState } from "react";
 
 export function OrderList() {
-  return <FormGroup></FormGroup>;
+  const [orderList, setOrderList] = useState();
+  return (
+    <Row>
+      <Col>
+        <Table>
+          <thead>
+            <tr>
+              <th>주문자명: {orderList.odrName}</th>
+              <th>주문 상태: {orderList.orderState}</th>
+              <th>상품 옵션: {orderList.orderOption}</th>
+              <th>상품 갯수: {orderList.count}</th>
+              <th>총 가격: {orderList.totalPrice}</th>
+              <th>
+                접수 시간:{" "}
+                {orderList.updateDttm
+                  ? orderList.updateDttm
+                  : orderList.inserDttm}
+              </th>
+            </tr>
+          </thead>
+        </Table>
+      </Col>
+    </Row>
+  );
 }
