666e452bea22fbe206ee7911ad518d1a4cf307eb
minki-jeon <minki.jeon.study@gmail.com>
2025-07-30 16:08:50 +0900

    로그아웃 기능 완료 (jwt 토큰 삭제)

--- frontend/src/feature/member/MemberLogout.jsx
+++ frontend/src/feature/member/MemberLogout.jsx
@@ -1,12 +1,19 @@
 import { Spinner } from "react-bootstrap";
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
 import { useNavigate } from "react-router";
+import { AuthenticationContext } from "../../common/AuthenticationContextProvider.jsx";
+import { toast } from "react-toastify";
 
 export function MemberLogout() {
+  // step2. Use the context (토큰 인증 context 호출)
+  const { logout } = useContext(AuthenticationContext);
   const navigate = useNavigate();
 
   useEffect(() => {
-    navigate("/");
+    logout();
+
+    toast("로그아웃 되었습니다.", { type: "success" });
+    navigate("/login");
   }, []);
 
   return <Spinner />;
