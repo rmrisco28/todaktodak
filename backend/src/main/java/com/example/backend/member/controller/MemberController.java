7614572c8e16468ae16072675d8706c5a9961579
jihoon0213 <jihun8138@gmail.com>
2025-08-13 20:46:06 +0900

    토스트 수정

--- backend/src/main/java/com/example/backend/member/controller/MemberController.java
+++ backend/src/main/java/com/example/backend/member/controller/MemberController.java
@@ -171,7 +171,7 @@ public class MemberController {
         }
         return ResponseEntity.ok().body(Map.of("message",
                 Map.of("type", "success",
-                        "text", "회원 정보가 수정되었습니다.")));
+                        "text", seq + "번 회원의 정보가 수정되었습니다.")));
     }
 
     // 회원 등록(관리자)
