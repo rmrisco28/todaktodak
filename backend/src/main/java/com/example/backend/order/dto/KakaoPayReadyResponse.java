dcba02310f13ff3fbe2d9dea012d3ee570814d42
minki-jeon <minki.jeon.study@gmail.com>
2025-08-19 21:13:57 +0900

    카카오페이api결제 오류 처리

--- backend/src/main/java/com/example/backend/order/dto/KakaoPayReadyResponse.java
+++ backend/src/main/java/com/example/backend/order/dto/KakaoPayReadyResponse.java
@@ -1,9 +1,12 @@
 package com.example.backend.order.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class KakaoPayReadyResponse {
     private String tid;
+    
+    @JsonProperty("next_redirect_pc_url")
     private String nextRedirectPcUrl;
 }
